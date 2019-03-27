8b54d1cb9a1132024ff35ded9da3a0dd4f9c7c42
Jakob Elias Dunst <jakob.dunst@student.tugraz.at>
2019-03-27 09:44:28 +0100

    GAL-004 added SharedPictureEspressoTest [DJ, GD]

--- morning07/app/src/androidTest/java/at/tugraz/morning07/SharePictureEspressoTest.java
+++ morning07/app/src/androidTest/java/at/tugraz/morning07/SharePictureEspressoTest.java
@@ -1,4 +1,31 @@
 package at.tugraz.morning07;
 
+import android.support.test.rule.ActivityTestRule;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
 public class SharePictureEspressoTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testButtonVisible() {
+        onView(withId(R.id.buttonShare)).check(matches(isDisplayed()));
+    }
 }
