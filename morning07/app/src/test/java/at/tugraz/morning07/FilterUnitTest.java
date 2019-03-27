c8fffc80c96913f2b34f904190fb354b6720daee
Maximilian Flicker <m.flicker@student.tugraz.at>
2019-03-27 09:51:52 +0100

    GAL-002 initial JunitTest [FM,SG]

---
+++ morning07/app/src/test/java/at/tugraz/morning07/FilterUnitTest.java
@@ -0,0 +1,42 @@
+package at.tugraz.morning07;
+
+import android.graphics.Bitmap;
+import android.media.ExifInterface;
+import android.media.Image;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+
+
+public class FilterUnitTest{
+
+    FilterImages filter;
+    Image image1;
+    Image image2;
+    Image image3;
+    Image[] imageArray = {image1, image2, image3};
+
+    @Before
+    public void setUp() throws IOException {
+        filter = new FilterImages();
+    }
+
+    @Test
+    public void filterByName(){
+        assertNotNull(filter);
+        assertTrue(filter.filterByName());
+    }
+
+}
