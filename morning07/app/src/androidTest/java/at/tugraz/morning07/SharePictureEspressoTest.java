1a752164d9d8cf87688cbff2934d08887f00111d
Jakob Elias Dunst <jakob.dunst@student.tugraz.at>
2019-04-03 10:39:17 +0200

    GAL-004 finished implementation of sharing function [DJ, GD]

--- morning07/app/src/androidTest/java/at/tugraz/morning07/SharePictureEspressoTest.java
+++ morning07/app/src/androidTest/java/at/tugraz/morning07/SharePictureEspressoTest.java
@@ -1,14 +1,19 @@
 package at.tugraz.morning07;
 
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.VerificationModes.times;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.anyIntent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -24,8 +29,27 @@ public class SharePictureEspressoTest {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+    @Rule
+    public IntentsTestRule<MainActivity> mActivity = new IntentsTestRule<MainActivity>(MainActivity.class) {
+        @Override
+        protected Intent getActivityIntent() {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND_MULTIPLE);
+            intent.setType("image/*");
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            return Intent.createChooser(intent, "Share images to..");
+        }
+    };
+
     @Test
     public void testButtonVisible() {
         onView(withId(R.id.shareButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testShareIntent() {
+        intended(anyIntent(), times(1));
+        onView(withId(R.id.shareButton)).check(matches(isDisplayed())).perform(click());
+        intended(anyIntent(), times(2));
     }
 }
