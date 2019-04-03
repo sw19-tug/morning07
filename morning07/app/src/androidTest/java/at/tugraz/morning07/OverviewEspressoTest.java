dd9deee219b083a7ff5af3b7353756568154d8af
Edmond Nrecaj <edmond.nrecaj@student.tugraz.at>
2019-04-03 10:01:41 +0200

    GAL-003 added Test Main Activity is being started [NE,RF]

--- morning07/app/src/androidTest/java/at/tugraz/morning07/OverviewEspressoTest.java
+++ morning07/app/src/androidTest/java/at/tugraz/morning07/OverviewEspressoTest.java
@@ -3,24 +3,21 @@ package at.tugraz.morning07;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Test;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
-public class ShowImageEspressoTest {
+public class OverviewEspressoTest {
     @Rule
-    public ActivityTestRule<Overview_stub> activityRule = new ActivityTestRule<>(Overview_stub.class);
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
     @Test
-    public void testMainAktivityisbeingStarted() {
-       onView(withId(R.id.imageList)).perform(click());
-       onView(withId(R.id.big_image)).check(matches(isDisplayed()));
+    public void testImageIsVisible() {
+        onView(withId(R.id.big_image)).check(matches(isDisplayed()));
     }
 }
