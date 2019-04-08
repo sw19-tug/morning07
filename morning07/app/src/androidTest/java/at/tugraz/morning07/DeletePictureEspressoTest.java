f198eccf28c620d0e56e88b3e9c00967d0fb27cd
Edmond Nrecaj <edmond.nrecaj@student.tugraz.at>
2019-04-08 17:49:24 +0200

    GAL-006 is delete button visible EspressoTest [NE,SM]

--- morning07/app/src/androidTest/java/at/tugraz/morning07/DeletePictureEspressoTest.java
+++ morning07/app/src/androidTest/java/at/tugraz/morning07/DeletePictureEspressoTest.java
@@ -2,26 +2,33 @@ package at.tugraz.morning07;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.GridView;
 
-import org.junit.Test;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.CoreMatchers.anything;
+
 
 @RunWith(AndroidJUnit4.class)
-public class ShowImageEspressoTest {
+public class DeletePictureEspressoTest {
+
     @Rule
-    public ActivityTestRule<BigImageActivity> activityRule = new ActivityTestRule<>(BigImageActivity.class);
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
-    public void testMainActivityIsBeingStarted() {
-        onView(withId(R.id.imageList)).perform(click());
-        onView(withId(R.id.big_image)).check(matches(isDisplayed()));
+    public void isVisibleDeleteButton(){
+        GridView view = activityRule.getActivity().photoGridView;
+
+        onData(anything()).inAdapterView(withId(R.id.photoGridView)).atPosition(0).perform(click());
+
+        onView(withId(R.id.deleteButton)).check(matches(isDisplayed()));
     }
 }
