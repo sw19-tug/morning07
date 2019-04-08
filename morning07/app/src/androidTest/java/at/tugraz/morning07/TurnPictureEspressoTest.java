69d814dadb9c1db5616677a2b34f741bce481fd3
Jakob Elias Dunst <jakob.dunst@student.tugraz.at>
2019-04-08 17:04:02 +0200

    GAL-008 TurnImageEspressoTest [DJ, RF]

---
+++ morning07/app/src/androidTest/java/at/tugraz/morning07/TurnPictureEspressoTest.java
@@ -0,0 +1,20 @@
+package at.tugraz.morning07;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public class TurnPictureEspressoTest {
+    @Rule
+    public ActivityTestRule<BigImageActivity> activityRule = new ActivityTestRule<>(BigImageActivity.class);
+    @Test
+    public void testTurnButtonVisible() {
+        onView(withId(R.id.turnButton)).check(matches(isDisplayed()));
+    }
+}
