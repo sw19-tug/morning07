72f3adb6fdb6aec8ee6fe9e9516616ce28c5513b
Jakob Elias Dunst <jakob.dunst@student.tugraz.at>
2019-04-10 11:47:25 +0200

    GAL-008 saveButton, saving turned image [DJ, RF]

--- morning07/app/src/main/java/at/tugraz/morning07/BigImageActivity.java
+++ morning07/app/src/main/java/at/tugraz/morning07/BigImageActivity.java
@@ -3,6 +3,8 @@ package at.tugraz.morning07;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,7 +13,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public class BigImageActivity extends AppCompatActivity {
@@ -19,6 +23,9 @@ public class BigImageActivity extends AppCompatActivity {
     int turnRatio = 0;
 
     private Button shareButton;
+    private Button saveButton;
+    private ImageView bigView;
+    private File imgFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,7 @@ public class BigImageActivity extends AppCompatActivity {
         setContentView(R.layout.big_image);
 
         this.shareButton = this.findViewById(R.id.shareButton);
+        this.saveButton = this.findViewById(R.id.saveButton);
         OnClickListenerShare shareListener = new OnClickListenerShare();
         ArrayList<Uri> imageUris = new ArrayList<>();
         File f = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
@@ -44,12 +52,12 @@ public class BigImageActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        ImageView bigView = (ImageView) findViewById(R.id.big_image);
+        this.bigView = (ImageView) findViewById(R.id.big_image);
         Intent intent = getIntent();
         if(intent != null){
             String message = intent.getStringExtra("filenpath");
             if(message != null) {
-                File imgFile = new File(message);
+                imgFile = new File(message);
                 Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
                 bigView.setImageBitmap(myBitmap);
             }
@@ -61,8 +69,27 @@ public class BigImageActivity extends AppCompatActivity {
 
     public void turn(View view)
     {
-        ImageView bigView = (ImageView) findViewById(R.id.big_image);
+        saveButton.setVisibility(View.VISIBLE);
+        //ImageView bigView = (ImageView) findViewById(R.id.big_image);
         turnRatio+=90;
         bigView.setRotation(turnRatio == 360 ? 0 : turnRatio);
+    }
+
+    public void save(View view) throws Exception
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(bigView.getRotation());
+        BitmapDrawable source = (BitmapDrawable)bigView.getDrawable();
+        Bitmap bm = Bitmap.createBitmap(source.getBitmap(), 0 ,0,
+                source.getBitmap().getWidth(), source.getBitmap().getHeight(), matrix, true);
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 0, bos);
+        byte[] bitmapdata = bos.toByteArray();
+
+        FileOutputStream fos = new FileOutputStream(imgFile);
+        fos.write(bitmapdata);
+        fos.flush();
+        fos.close();
     }
 }
