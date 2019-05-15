e5863594ed66dce1b6fa4ab1a7e0722d8b7d91c4
Georg Schuh <g.schuh@student.tugraz.at>
2019-05-15 10:20:54 +0200

    GAL_007 espresso test taking picture view [SG,SS]

--- morning07/app/src/androidTest/java/at/tugraz/morning07/AddPictureEspressoTest.java
+++ morning07/app/src/androidTest/java/at/tugraz/morning07/AddPictureEspressoTest.java
@@ -5,27 +5,35 @@ import android.app.Instrumentation;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.core.internal.deps.guava.collect.Iterables;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.rule.GrantPermissionRule;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 import static android.app.Activity.RESULT_OK;
+import static android.content.Context.CAMERA_SERVICE;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -33,8 +41,6 @@ import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class AddPictureEspressoTest {
-    @Rule
-    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Rule
     public GrantPermissionRule permissionRule = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
@@ -48,8 +54,18 @@ public class AddPictureEspressoTest {
     }
 
     @Test
-    public void testCameraWorkflow() {
-        //source: https://guides.codepath.com/android/UI-Testing-with-Espresso#stubbing-out-the-camera
+    public void testClickPhotoButton() {
+        onView(withId(R.id.openCameraActionButton)).perform(click());
+    }
+
+    @Test
+    public void testCameraOpen() {
+        intended(hasAction(MediaStore.ACTION_IMAGE_CAPTURE));
+    }
+
+    //source: https://guides.codepath.com/android/UI-Testing-with-Espresso
+    @Test
+    public void testTakingPictureScenario() {
         Bitmap icon = BitmapFactory.decodeResource(
                 InstrumentationRegistry.getTargetContext().getResources(),
                 R.mipmap.ic_launcher);
@@ -59,7 +75,9 @@ public class AddPictureEspressoTest {
         Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
 
         intending(toPackage("com.android.camera2")).respondWith(result);
+
         onView(withId(R.id.openCameraActionButton)).perform(click());
+
         intended(toPackage("com.android.camera2"));
     }
 }
