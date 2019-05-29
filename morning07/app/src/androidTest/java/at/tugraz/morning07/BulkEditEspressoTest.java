aa98596e69aa840173920839139a9399fdb73b35
Markus Schleifer <mschleifer@student.tugraz.at>
2019-05-29 11:28:49 +0200

    GAL-010 Espresso Test for Toolbar [SG, SM]

--- morning07/app/src/androidTest/java/at/tugraz/morning07/BulkEditEspressoTest.java
+++ morning07/app/src/androidTest/java/at/tugraz/morning07/BulkEditEspressoTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.longClick;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
@@ -28,6 +30,11 @@ public class BulkEditEspressoTest {
     public void testSelectImage() {
         onData(anything()).inAdapterView(withId(R.id.photoGridView)).atPosition(0).perform(longClick());
         onData(anything()).inAdapterView(withId(R.id.photoGridView)).atPosition(1).perform(longClick());
-        onView(withId(R.id.bulk_delete_images)).check(matches(isDisplayed()));
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        onView(withText("Turn Left")).check(matches(isDisplayed()));
+        onView(withText("Turn Right")).check(matches(isDisplayed()));
+        onView(withText("Delete")).check(matches(isDisplayed()));
+        onView(withText("Share")).check(matches(isDisplayed()));
     }
 }
